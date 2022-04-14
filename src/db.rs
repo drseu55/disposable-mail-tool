df337d15791f377919251724006a4b01cad9260b
drseu55 <alpics1337@gmail.com>
2022-04-14 19:46:03 +0300

    feat: Move index creation in db file

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,8 @@
-use mongodb::{options::ClientOptions, Client};
+use bson;
+use mongodb::{options::ClientOptions, options::IndexOptions, Client, Collection, IndexModel};
+use std::time;
+
+use crate::mails;
 
 const URL: &str = "mongodb://localhost";
 const PORT: &str = "27017";
@@ -11,4 +15,23 @@ pub async fn connect() -> Result<Client, mongodb::error::Error> {
     let client = Client::with_options(client_options)?;
 
     Ok(client)
+}
+
+pub async fn create_index(
+    email_users: &Collection<bson::Document>,
+) -> Result<(), mails::MailError> {
+    // Create an index in collection that will automatically
+    // delete document after 60 minutes
+    let index_key = bson::doc! { "createdAt": 1 };
+    let index_options = IndexOptions::builder()
+        .expire_after(Some(time::Duration::new(3600, 0)))
+        .build();
+    let index_model = IndexModel::builder()
+        .keys(index_key)
+        .options(index_options)
+        .build();
+
+    email_users.create_index(index_model, None).await?;
+
+    Ok(())
 }
